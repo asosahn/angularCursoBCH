457464d49aadcb4289acf9730096792bc5706387
Andres Sosa <ramonsosadiaz@gmail.com>
2019-09-04 09:07:49 -0600

    forma

--- src/app/components/forma/forma.component.ts
+++ src/app/components/forma/forma.component.ts
@@ -2,6 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+interface Cliente {
+  name: string;
+  lastName: string;
+  gender: string;
+  email: string;
+  country: string;
+}
+
 @Component({
   selector: 'app-forma',
   templateUrl: './forma.component.html',
@@ -9,6 +17,13 @@ import { NgForm } from '@angular/forms';
 })
 export class FormaComponent implements OnInit {
   countries: any[] = [];
+  cliente: Cliente = {
+    name: 'Andres',
+    lastName: 'Sosa',
+    email: 'ramonsosadiaz@gmail.com',
+    gender: 'm',
+    country: 'HN'
+  };
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
@@ -21,7 +36,14 @@ export class FormaComponent implements OnInit {
   }
 
   guardarFormulario( formulario: NgForm ) {
-    console.log(formulario);
+    if (formulario.valid) {
+      // request al api
+      console.log('formulario válido');
+    } else {
+      console.log('formulario inválido');
+    }
   }
 
 }
+
+
