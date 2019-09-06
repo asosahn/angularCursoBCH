7d883ec713b30b769ff7da3abf5acbe584454065
Andres Sosa <ramonsosadiaz@gmail.com>
2019-09-06 01:41:12 -0600

    cleaning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,10 +11,6 @@ import { NgxSpinnerService } from 'ngx-spinner';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnDestroy {
-  userActivity;
-  userInactive: Subject<any> = new Subject();
-  showLoading: Subscription;
-  private loading$ = new Subject<any>();
   showOrHideLoading: Subscription;
  constructor(private spinner: NgxSpinnerService, private alertService: AlertasService) {
   this.showOrHideLoading = this.alertService.loadingSubscription().subscribe(
@@ -26,27 +22,7 @@ export class AppComponent implements OnDestroy {
         }
       })
     );
-  // this.setTimeout();
-  // this.userInactive.subscribe(() => {
-  //   Swal.fire({
-  //     title: 'Error!',
-  //     text: 'Sesión de mas de 20 seg sin actividad',
-  //     type: 'error',
-  //     confirmButtonText: 'Aceptar'
-  //   });
-  // });
  }
-
-//  setTimeout() {
-//   this.userActivity = setTimeout(() => this.userInactive.next(undefined), 20000);
-// }
-
-// @HostListener('window:mousemove')
-// @HostListener('window:keyup')
-// refresUserState() {
-//   clearTimeout(this.userActivity);
-//   this.setTimeout();
-// }
 
 ngOnDestroy(): void {
   this.showOrHideLoading.unsubscribe();
