bd56334681ed7877c0b6d78a5071d6440ad61d28
Andres Sosa <ramonsosadiaz@gmail.com>
2019-09-06 02:56:55 -0600

    pilas auth

--- src/app/services/authguard/auth.guard.ts
+++ src/app/services/authguard/auth.guard.ts
@@ -8,8 +8,9 @@ import { Observable } from 'rxjs';
 })
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
+
   canActivate(): Observable<boolean> | Promise<boolean> | boolean {
-    if (this.authService.isLoggedIn) {
+    if (this.authService.isLoggedIn()) {
       return true;
     }
     this.router.navigateByUrl('/login');
